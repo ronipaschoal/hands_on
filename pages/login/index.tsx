68f3e05e6c54baf0c3a8abff38f875884731f8ea
roni.paschoal <roni.paschoal@ilog.com.br>
2021-11-14 18:07:57 -0300

    Criada validação no formulário de login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -7,8 +7,39 @@ import Header from '../../component/Header';
 import Button from '../../component/Button';
 
 import {loginService} from '../../services/auth';
+import { useState } from 'react';
 
 const Login: NextPage = () => {
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  
+  const validateEmail = (email: string) => {
+    const regex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+    if(!email) {
+      setEmailError('O e-mail deve ser preenchido');
+      return false;
+    }
+    if(!regex.test(email)) {
+      setEmailError('Favor digitar um e-mail válido');
+      return false;
+    }
+    setEmailError('');
+    return true;
+  }
+
+  const validatePassword = (password: string) => {
+    if(!password) {
+      setPasswordError('A senha deve ser preenchida');
+      return false;
+    }
+    if(password.length < 6) {
+      setPasswordError('A senha deve possuir ao menos 6 caracteres');
+      return false;
+    }
+    setPasswordError('');
+    return true;
+  }
 
   const handleSubimit = (event: React.SyntheticEvent) => {
     
@@ -18,23 +49,28 @@ const Login: NextPage = () => {
       email: { value: string };
       password: { value: string };
     };
-    const email = target.email.value;
-    const password = target.password.value;
-
-    loginService({ email: email, password: password });
+    
+    const validEmail = validateEmail(target.email.value);
+    const validPassword = validatePassword(target.password.value);
+    
+    if(validEmail && validPassword) {
+      loginService({ email: target.email.value, password: target.password.value });
+    }
 
   }
 
   return (
     <>
       <Header />
-      <main className={styles.login}>
+      <main lang="pt-BR" className={styles.login}>
         <form onSubmit={handleSubimit}>
           <h1>Faça seu login</h1>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">E-mail:</label>
           <input id="email" name="email" type="text" placeholder="Digite seu e-mail" />
+          <span className={styles.invalid}>{emailError}</span>
           <label htmlFor="password">Senha:</label>
           <input id="password" name="password" type="password" placeholder="Digite sua senha" />
+          <span className={styles.invalid}>{passwordError}</span>
           <Button>Entrar</Button>
         </form>
         <Footer botton={true} />
